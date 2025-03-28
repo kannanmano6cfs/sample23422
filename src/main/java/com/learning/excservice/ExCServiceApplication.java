9f3dfc0e8df33126eb1338ee7e07ff666f3b0095
Kannan Manoharan <161038652+kannanmano6cfs@users.noreply.github.com>
2025-03-28 09:16:00 +0530

    Add files via upload

---
+++ src/main/java/com/learning/excservice/ExCServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learning.excservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExCServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExCServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learning/excservice/ExCServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.excservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExCServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
